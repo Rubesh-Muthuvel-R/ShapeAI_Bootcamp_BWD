e54100a9858e2e8d8f31063373f8458534bd6665
Rubesh-Muthuvel-R <83689801+Rubesh-Muthuvel-R@users.noreply.github.com>
2021-06-07 02:34:24 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        We had a very grate experience on attending and successfully completing
+        this bootcamp.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
